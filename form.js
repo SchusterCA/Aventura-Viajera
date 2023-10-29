76f7feb852a96a470765c763e9cbc862660112cd
Candela Schuster <148298787+SchusterCA@users.noreply.github.com>
2023-10-29 16:53:42 -0300

    Add files via upload

---
+++ form.js
@@ -0,0 +1,17 @@
+var formulario = document.getElementById('formulario');
+var campoCorreo = document.getElementById('correo');
+var campoTelefono = document.getElementById('telefono');
+
+formulario.addEventListener('submit', function(event) {
+
+    if (!campoCorreo.value.match(/^\S+@\S+.\S+$/)) {
+        alert('ingresa un correo válido.');
+        event.preventDefault(); 
+    }
+
+
+    if (!campoTelefono.value.match(/^\d{10}$/)) {
+        alert('ingresa un número de teléfono válido');
+        event.preventDefault(); 
+    }
+});
